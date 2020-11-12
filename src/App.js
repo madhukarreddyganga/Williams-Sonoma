40710b40ff8f92efe81bbc06f4f2af005fbaa749
madhukarreddyganga <45326658+madhukarreddyganga@users.noreply.github.com>
2020-11-11 20:16:38 -0500

    updated with map with name

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App extends React.Component {
       .get("/services/catalog/v4/category/shop/new/all-new/index.json")
       .then((response) => {
         console.log(response);
-        console.log(response.data.groups[0].price.regular);
+        console.log(response.data.groups[0].name);
         this.setState({
-          names: response.data.name,
+          names: response.data.groups.name,
           images: response.data.groups.images,
-          id: response.data.id
+          id: response.data.groups.id
         });
       })
       .catch((error) => {
@@ -32,6 +32,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2>Start editing to see some magic happen!</h2>
+        {this.state.images.map((name) => {
+          return <div>{name}</div>;
+        })}
       </div>
     );
   }
